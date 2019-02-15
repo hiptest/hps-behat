84de91ea80482bf3b7ebb7eac7ad5bc2a676178d
Jens Christian Asving Donslund <jd@Jenss-MacBook-Air.local>
2019-02-15 15:34:56 +0100

    Featurecontext

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -11,7 +11,7 @@ class FeatureContext implements SnippetAcceptingContext {
     $this->actionwords = new Actionwords();
   }
 
-
+// test
   /**
    * @When /^I start the coffee machine using language "(.*)"$/
    */
